87c08f2d832fc6efedfcc0ad09e0e51405c8419e
shivanand-k <shivanandcrew034@gmail.com>
2026-02-05 18:02:34 +0530

    initialized client

---
+++ client/src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="card bg-base-100 w-96 shadow-sm">
+  <div className="card-body">
+    <h2 className="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+  );
+}
