81e48198647e8a26a91078dea94857f5b9ae298c
shivanand-k <shivanandcrew034@gmail.com>
2026-02-06 21:13:47 +0530

    updated-queries, updated-naming

--- server/services/analytic_service.py
+++ server/services/analytic_service.py
@@ -16,18 +16,18 @@ def overview_analytic(db: Session):
         stmt1 = (
             select(
                 func.sum(Investment.investment_amount).label("total_invested"),
-                func.sum(Investment.investment_amount * Investment.return_percent).label("overall_profit_loss"),
-                func.max(Investment.return_percent * Investment.investment_amount).label("single_highest_profit"),
-                func.min(Investment.return_percent * Investment.investment_amount).label("single_lowest_profit")
+                func.sum(Investment.investment_amount * Investment.return_percent).label("net_profit_loss"),
+                func.max(Investment.return_percent * Investment.investment_amount).label("single_investment_highest_profit"),
+                func.min(Investment.return_percent * Investment.investment_amount).label("single_investment_lowest_profit")
             ).where(Investment.dataset_id == dataset_id)
         )
         over_result = db.execute(stmt1).one()
-        profit_loss = (Investment.return_percent * Investment.investment_amount).label("profit_loss")
+        net_profit_loss = (Investment.return_percent * Investment.investment_amount).label("net_profit_loss")
         stmt2 = (
             select(
                 Investment.investment_type, Investment.investment_amount, Investment.return_percent,
-                profit_loss
-            ).where(Investment.dataset_id == dataset_id).order_by(profit_loss)
+                net_profit_loss
+            ).where(Investment.dataset_id == dataset_id).order_by(net_profit_loss)
         )
         table_result = db.execute(stmt2).mappings().all()
         return {
@@ -46,11 +46,14 @@ def distribution_analytic(db: Session):
     try:
         # dataset_id = get_dataset_id_access_token(token)
         total_invested_per_type = func.sum(Investment.investment_amount).label('total_invested_per_type')
+        avg_returns_per_type = (func.sum(Investment.investment_amount * Investment.return_percent) /
+                                func.sum(Investment.investment_amount)).label('Avg_returns_per_type')
         stmt = (
             select(
                 Investment.investment_type,
-                func.sum(Investment.investment_amount * Investment.return_percent).label('profit_loss'),
-                total_invested_per_type
+                func.sum(Investment.investment_amount * Investment.return_percent).label('profit_loss_per_type'),
+                total_invested_per_type,
+                avg_returns_per_type
             )
             .where(Investment.dataset_id == dataset_id)
             .group_by(Investment.investment_type)
@@ -89,10 +92,12 @@ def by_type_analytic(db: Session):
 def trends_analytic(db: Session):
     try:
         # dataset_id = get_dataset_id_access_token(token)
-        profit_loss = func.sum(Investment.investment_amount * Investment.return_percent).label('profit_loss')
+        total_invested_per_year = func.sum(Investment.investment_amount).label('total_invested_per_year')
+        profit_loss_per_year = (func.sum(Investment.investment_amount * Investment.return_percent)
+                                .label('profit_loss_per_year'))
         year_wise = func.to_char(Investment.investment_date, 'YYYY').label('year_wise')
         stmt = (
-            select(year_wise, profit_loss)
+            select(year_wise, profit_loss_per_year, total_invested_per_year)
             .where(Investment.dataset_id == dataset_id)
             .group_by(year_wise)
             .order_by(year_wise)
